6e386986dee694008fc108fa3a53a02b1de993cc
bycms <160313509+bycms@users.noreply.github.com>
2024-10-02 19:52:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ const hidContent = document.getElementById("hidZone");
 const hidButton = document.getElementById("hidCancel");
 hidContent.style.display = "none";
 
-showButton.onClick =()=> function {
+showButton.onClick = function() {
     if (hidPasswordInput.value == "MYcid994.") {
         hidContent.style.display = "block";
     } 
 }
 
-hidButton.onClick =()=> function {
+hidButton.onClick = function() {
     hidContent.style.display = "block";
 }
