29549023c11629f38bd4f01d60b803ba8abfdceb
bycms <160313509+bycms@users.noreply.github.com>
2024-10-02 19:40:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,18 +3,18 @@
     the HIDDEN ZONE
     ONLY.
 */
-const showButton = document.getElementById("hid Confirm");
+const showButton = document.getElementById("hidConfirm");
 const hidPasswordInput = document.getElementById("hidPassword");
 const hidContent = document.getElementById("hidZone");
 const hidButton = document.getElementById("hidCancel");
 hidContent.style.display = "none";
 
-showButton.addEventListener("click", function =()=> {
+showButton.onClick =()=> function {
     if (hidPasswordInput.value == "MYcid994.") {
         hidContent.style.display = "block";
     } 
-})
+}
 
-hidButton.addEventListener("click", function =()=> {
+hidButton.onClick =()=> function {
     hidContent.style.display = "block";
-})
+}
